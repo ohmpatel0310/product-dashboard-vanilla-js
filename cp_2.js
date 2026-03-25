1836b8d871de1cdebef7b1bf61702068331953a6
Ohm Patel <ohmp3@usf.edu>
2026-03-25 13:35:44 -0400

    Add fetchProductsAsync With async/await and try/catch Error Handling

--- cp_2.js
+++ cp_2.js
@@ -1,4 +1,4 @@
-// Step 3: 
+// Step 3: Defining fetchProductsThen function using Promise-based syntax
 
 function fetchProductsThen() {
     fetch(`https://www.course-api.com/javascript-store-products`)
@@ -17,4 +17,25 @@ function fetchProductsThen() {
             console.error(`Fetch error (Promise Based):`, error);
         });
 
+}
+
+
+// Step 4: Defining fetchProductsAsync function using async/await syntax
+
+async function fetchProductsAsync() {
+    try {
+        const response = await fetch(`https://www.course-api.com/javascript-store-products`);
+
+        if (!response.ok) {
+            throw new Error(`Network response was not ok`);
+        }
+
+        const products = await response.json();
+
+        displayProducts(products);
+
+    } catch (error) {
+
+        handleError(error);
+    }
 }
